80e39abfde8156b439d3378004d31591a32503bb
Samant M Patil <samantpatil.0425@gmail.com>
2026-07-11 17:17:04 +0530

    feat: update PPLOG theme, navigation host and MainActivity

--- app/src/main/java/com/pplog/app/MainActivity.kt
+++ app/src/main/java/com/pplog/app/MainActivity.kt
@@ -5,14 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.pplog.app.ui.navigation.PPLOGNavHost
 import com.pplog.app.ui.theme.PPLOGTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,33 +17,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PPLOGTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        Greeting("PPLOG")
-                    }
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    PPLOGNavHost(navController = rememberNavController())
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PPLOGTheme {
-        Greeting("PPLOG")
     }
 }
